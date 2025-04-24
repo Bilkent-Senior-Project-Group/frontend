0e1ac8722d46a6c6014fc449f6fb254824930954
Yaşar <yasar.tatlicioglu@ug.bilkent.edu.tr>
2025-04-24 13:01:44 +0300

    production env

--- company-hub-service/src/config/apiConfig.js
+++ company-hub-service/src/config/apiConfig.js
@@ -3,5 +3,5 @@
  * Edit this file to change the API URL according to your local environment
  */
 
-export const API_URL = 'http://localhost:5133';
-export const SEARCH_API_URL = 'http://127.0.0.1:8001';
+export const API_URL = 'companyhubservicebalancer-1799722945.eu-north-1.elb.amazonaws.com';
+export const SEARCH_API_URL = 'searchservice-1646147650.eu-north-1.elb.amazonaws.com';
