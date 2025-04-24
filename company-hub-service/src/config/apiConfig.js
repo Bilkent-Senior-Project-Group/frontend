cf891899b614df507fc459b0fa453eacdbf01057
Yaşar <yasar.tatlicioglu@ug.bilkent.edu.tr>
2025-04-24 15:15:49 +0300

    url mistake fixed

--- company-hub-service/src/config/apiConfig.js
+++ company-hub-service/src/config/apiConfig.js
@@ -3,5 +3,5 @@
  * Edit this file to change the API URL according to your local environment
  */
 
-export const API_URL = 'companyhubservicebalancer-1799722945.eu-north-1.elb.amazonaws.com';
-export const SEARCH_API_URL = 'searchservice-1646147650.eu-north-1.elb.amazonaws.com';
+export const API_URL = 'http://companyhubservicebalancer-1799722945.eu-north-1.elb.amazonaws.com';
+export const SEARCH_API_URL = 'http://searchservice-1646147650.eu-north-1.elb.amazonaws.com';
