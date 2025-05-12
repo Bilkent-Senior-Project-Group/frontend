e56e92d9abf34dfaf01f170280301c7284afde9f
Ece Beyhan <ecebeyhan152@gmail.com>
2025-05-12 10:00:33 +0300

    localhost not used

--- company-hub-service/src/config/apiConfig.js
+++ company-hub-service/src/config/apiConfig.js
@@ -5,3 +5,6 @@
 
 export const API_URL = 'http://companyhubservicebalancer-1799722945.eu-north-1.elb.amazonaws.com';
 export const SEARCH_API_URL = 'http://searchservice-1646147650.eu-north-1.elb.amazonaws.com';
+
+//export const API_URL = 'http://localhost:5133';
+//export const SEARCH_API_URL = 'http://127.0.0.1:8000';
